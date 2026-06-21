6cff37097a4f0aaa92505169b87a8fb9eeba48ab
Ali Zahid Alavi <aalavi2401@conestogac.on.ca>
2026-06-20 23:04:13 -0400

    Initial commit

---
+++ DSA_Assign_1/queue.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+struct User {
+	string username;
+	int level;
+	string faction;
+};
+
+struct Node {
+
+	struct User data;
+
+};
+
+struct Queue {
+
+	struct Node head;
+	struct Node tail;
+
+};
+
+int main(void) {
+
+
+
+	return 0;
+}
