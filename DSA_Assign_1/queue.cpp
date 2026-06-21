75b59fc519e417b0ce9152081c8280a3e29ec425
Ali Zahid Alavi <aalavi2401@conestogac.on.ca>
2026-06-20 23:52:42 -0400

    added comments

--- DSA_Assign_1/queue.cpp
+++ DSA_Assign_1/queue.cpp
@@ -2,80 +2,84 @@
 #include <ctime>
 using namespace std;
 
+//user profile struct
 struct User {
-	string username;
-	int level;
-	string faction;
+	string username; //username member
+	int level; // level (1-60) member
+	string faction; //faction member
 };
 
+//Node struct for queue
 struct Node {
 
-	struct User data;
-	struct Node* next;
+	struct User data; //Variable representing a 'User' object
+	struct Node* next; //Link to next node in queue
 };
 
+//Globally declared node pointers head and tail for the queue
 static struct Node* head;
 static struct Node* tail;
 
 void initQueue() {
-	head = tail = NULL;
+	head = tail = NULL; //Initiating head and tail to NULL
 }
 
 bool checkEmpty() {
-	return (head == NULL);
+	return (head == NULL); //Checks if head is null, i.e. if queue is empty
 }
 
+//Function to add a new node to queue
 void enqueue(struct Node*& head, struct User data) {
-	struct Node* newNode = new Node;
+	struct Node* newNode = new Node; //Allocates memory for a new node
 
-	newNode->data = data;
+	newNode->data = data; //Assigns the User object data to the new node's data member
 
 	if (head == NULL) {
-		head = tail = newNode;
+		head = tail = newNode; //If queue is empty, assigns the new node to both head and tail
 	}
 	else {
-		tail->next = newNode;
-		tail = newNode;
+		tail->next = newNode; //If queue is not empty, makes the current tail point to the new tail
+		tail = newNode; //Updates the tail of the queue
 	}
-	newNode->next = NULL;
+	newNode->next = NULL; //Makes the new node point to NULL
 
 }
 
 void dequeue(struct Node*& head) {
 	if (checkEmpty()) {
-		cout << "No nodes to dequeue!" << endl;
+		cout << "No nodes to dequeue!" << endl; //Displays message to user if queue is empty
 		return;
 	}
 
-	Node* temp = head;
-	head = head->next;
-	delete temp;
-	if (head == NULL) tail = NULL;
+	Node* temp = head; //Pointer to current head
+	head = head->next; //Updates head to new node
+	delete temp; //Free memory
+	if (head == NULL) tail = NULL; //Updates tail if the queue is now empty.
 
 	return;
 }
 
 int populateQueue(struct Node*& head) {
-	cout << "How many users would you like to queue?: " << endl;
+	cout << "How many users would you like to queue?: " << endl; //Prompts user for input on how many nodes they'd like to add
 	int users;
 	cin >> users;
 
-	string alphabet = "abcdefghijklmnopqrstuvwxyz1234567890";
+	string alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"; //String holding alphabets and numbers
 
 
-	for (int i = 0; i < users; i++) {
-		string username = "";
+	for (int i = 0; i < users; i++) { //For loop that runs as many times as needed depending on number of users to add
+		string username = ""; 
 		string faction;
 		int level;
 
-		level = (rand() % 60) + 1;
+		level = (rand() % 60) + 1; //Randomly assigns a number from 1 - 60
 		for (int j = 0; j < 10; j++) {
-			int randomNum = rand() % 36;
+			int randomNum = rand() % 36; //Randomly picks a number between 1 - 36 to pick from the alphabet and numbers.
 			username += alphabet[randomNum];
 		}
 
-		int randomFaction = rand() % 3;
-		switch (randomFaction) {
+		int randomFaction = rand() % 3; //Generate number between 0-2
+		switch (randomFaction) { //Switch to assign faction depending on the number generated.
 		case 0:
 			faction = "Red";
 			break;
@@ -86,14 +90,14 @@ int populateQueue(struct Node*& head) {
 			faction = "Green";
 			break;
 		default:
-			cout << "Switch failed!" << endl;
+			cout << "Switch failed!" << endl; //Exception handling.
 			return 1;
 		}
-		User user;
+		User user; //Creates user object and assigns all the values
 		user.username = username;
 		user.level = level;
 		user.faction = faction;
-		enqueue(head, user);
+		enqueue(head, user); //Queues nodes.
 	}
 	return 0;
 
@@ -101,11 +105,11 @@ int populateQueue(struct Node*& head) {
 
 int main(void) {
 
-	srand(time(NULL));
-	initQueue();
-	populateQueue(head);
+	srand(time(NULL)); //Seeing random number generator
+	initQueue(); //Initiating queue
+	populateQueue(head); //Calling populate function to create and assign random values to the queue
 
-	while (head != NULL) {
+	while (head != NULL) { //Loop to dequeue and print out all nodes
 		cout << "--------------" << endl;
 		cout << "Username: " << head->data.username << endl;
 		cout << "Faction: " << head->data.faction << endl;
